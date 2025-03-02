d615e71fb58bd07b0042e38e053af542d7330a9b
Pedro <pedro_schunck@outlook.com.br>
2025-03-02 16:04:36 -0300

    salvando melhorias

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import bannerImg from '../../assets/images/fundo_hogwarts.png'
+import Button from '../Button'
+import Tag from '../Tag'
+
+import { Banner, Infos } from './styles'
+
+const Hero = () => (
+  <Banner style={{ backgroundImage: `url(${bannerImg})` }}>
+    <div className="container">
+      <div>
+        <Tag>RPG</Tag>
+        <Tag>PSR</Tag>
+      </div>
+
+      <Infos>
+        <h2>Jogwarts Legacy</h2>
+        <p>
+          <span>De R$ 250,00</span>
+          Por R$ 190,00
+        </p>
+        <Button
+          type="button"
+          title="Clique aqui para adicionar este jogo ao carrinho"
+          variant="primary"
+        >
+          Adicionar ao carrinho
+        </Button>
+      </Infos>
+    </div>
+  </Banner>
+)
+
+export default Hero
